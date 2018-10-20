5eac2449ff434ec503271729db8b5fa75376cae2
Anders Hoff <inconvergent@gmail.com>
2018-10-20 14:19:55 +0200

    intro app

---
+++ views/js/intro.js
@@ -0,0 +1,48 @@
+/* global createCanvas, mouseIsPressed, fill, mouseX, mouseY, ellipse,
+   windowHeight, windowWidth, cos, sin, random, vec, rndInCirc
+*/
+
+// some global variables that can be accessed by all functions below.
+let state, mid, win;
+
+
+function init(n) {
+}
+
+
+function setup() {
+  // use a setup function to initialize the canvas, as well as to
+  // assign values to other variables you will need for your sketch.
+  //
+  // p5.js functions must be called from inside setup(), or draw(). otherwise
+  // they are undefined
+
+  // canvas size
+  win = new vec(1000, 1000);
+
+  // the coordinate of the middle of the canvas
+  mid = win.copy().mult(0.5);
+
+  // make the canvas of size win
+  createCanvas(win.x, win.y);
+
+  // assign other values
+  init(10);
+}
+
+function draw() {
+  //the draw function is called for every animation step automatically.
+  //if you want to clear the canvas between each time step use: clear()
+  //
+  //see what happens of you comment this in or out:
+  //clear();
+
+  // get the coordinate of the mouse pointer
+  const mouse = new vec(mouseX, mouseY);
+
+  // draw a circle at the moise position.
+  drawCirc([mouse]);
+
+  //state = state.points.map(o => {
+  //});
+}
